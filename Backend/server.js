79e0ff3df9823ec9254d66fff3f9a168d0629d91
Your Name <apatel7748@gmail.com>
2026-02-20 21:03:20 +0530

    server fixd

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,21 @@
-// const dotenv = require("dotenv");
-// dotenv.config();
 import dotenv from "dotenv";
-dotenv.config({ path: ".env" });
+dotenv.config();
 import app from './src/app.js';
 
-// db connent
+// db connect
 import connectToDatabase from './src/db/Database.js';
 connectToDatabase()
 
-
+app.get("/", (req, res) => {
+    res.send("Hello World! Server is live.");
+})
 
 // server
-const port = process.env.PORT;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+const port = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    })
+}
+
+export default app;
